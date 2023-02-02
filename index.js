16821fc9408a9ac58930751c967e7301ca72b110
Lauren <halpertln@gmail.com>
2023-02-02 14:41:24 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee ={
+    name: "Lauren",
+    streetAddress: "123 Main St.",
+}
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    const newEmployee = {...object};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    return object;
+}
+function deleteFromEmployeeByKey(object, key, value) {
+    const copyEmployee = {...object};
+    delete copyEmployee[key];
+    return copyEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(object, key, value) {
+    delete object[key];
+    return object;
+}
